6ed15eb39e9f7b21119578937fac9662c937fb36
huenupe <huenupe.benjaminx@gmail.com>
2023-06-30 21:21:45 -0400

    ed

--- CreandoApi/MasDjango/core/urls.py
+++ CreandoApi/MasDjango/core/urls.py
@@ -5,7 +5,7 @@ app_name = 'informes'
 
 urlpatterns = [
     path('', home, name="home"),
-    path('form-producto', form_producto, name="form_producto"),
+    path('form-producto/', form_producto, name="form_producto"),
     path('form-mod-producto/<id>', form_mod_producto, name="form_mod_producto"),
     path('form-del-producto/<id>', form_del_producto, name="form_del_producto"),
     path('descargar-informe/', descargar_informe, name='descargar_informe'),
