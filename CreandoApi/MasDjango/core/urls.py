be6d3d86087b97680e35004e7f5f8ad440efa911
“jaastorgar” <ja.astorgar@duocuc.cl>
2023-06-30 23:15:55 -0400

    xd

--- CreandoApi/MasDjango/core/urls.py
+++ CreandoApi/MasDjango/core/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import home, form_producto, form_mod_producto, form_del_producto, descargar_informe, Pag_principal, seccion_carnes, seccion_licores, seccion_snacks, seccion_verduras
 
-app_name = 'informes'
 
 urlpatterns = [
     path('', home, name="home"),
@@ -10,8 +9,8 @@ urlpatterns = [
     path('form-del-producto/<id>', form_del_producto, name="form_del_producto"),
     path('descargar-informe/', descargar_informe, name='descargar_informe'),
     path('Pag_principal/', Pag_principal, name="Pag_principal"),
-    path('seccion_carnes',seccion_carnes, name="seccion_carnes"),
-    path('seccion_licores',seccion_licores,name="seccion_licores"),
-    path('seccion_snacks',seccion_snacks, name="seccion_snacks"),
-    path('seccion_verduras',seccion_verduras, name="seccion_verduras"),
+    path('seccion_carnes/',seccion_carnes, name="seccion_carnes"),
+    path('seccion_licores/',seccion_licores,name="seccion_licores"),
+    path('seccion_snacks/',seccion_snacks, name="seccion_snacks"),
+    path('seccion_verduras/',seccion_verduras, name="seccion_verduras"),
 ]
